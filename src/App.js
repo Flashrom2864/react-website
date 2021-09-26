888cbba401823dd25804f6edde8ba11a47574aca
Naeem Mannan <nmannan97@gmail.com>
2021-09-25 17:20:24 -0700

    Hover effect

--- src/App.js
+++ src/App.js
@@ -10,16 +10,19 @@ import SignUp from './components/pages/SignUp';
 
 function App() {
   return (
-    
+      
       <Router>
         <Navbar />
+        <div className = 'container' id = 'container'> 
         <Switch>
           <Route path='/' exact component={Home} />
           <Route path='/services' component={Services} />
           <Route path='/products' component={Products} />
           <Route path='/sign-up' component={SignUp} />
         </Switch>
+        </div>
       </Router>
+      
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {CSSTransition} from 'react-transition-group';
+
+
+<CSSTransition
+  timeout={1000}
+  classnames = 'container'
+  unmountOnExit
+>
+    <p>Welcome</p>
+
+</CSSTransition>
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-
